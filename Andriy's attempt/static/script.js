12fa95579f476f371554df15a16e67b401b34634
Archibald <archibaldkami@proton.me>
2024-11-21 22:28:13 +0200

    remove non-essential files

--- Andriy's attempt/static/script.js
+++
@@ -1,9 +0,0 @@
-let headerBurgerBtn = document.querySelector(".header__burger")
-let headerNav = document.querySelector(".header__nav")
-let body = document.body
-
-headerBurgerBtn.addEventListener("click", () => {
-	headerBurgerBtn.classList.toggle("active")
-	headerNav.classList.toggle("active")
-	body.classList.toggle("fixed")
-})
